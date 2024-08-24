57db715afa26d1f33064da3a76933c65a7c45093
Merna Bassem <mernabassem21@gmail.com>
2024-08-24 23:32:50 +0300

    needs model

---
+++ DB/Models/need.model.js
@@ -0,0 +1,45 @@
+/**
+ * id 
+ * familyId
+ * need 
+ * quantity
+ * member // optional
+ * price // optional
+ */
+
+import mongoose from "mongoose";
+
+const { Schema, model } = mongoose;
+
+const needSchema = new mongoose.Schema(
+  {
+    familyId: {
+      type: Schema.Types.ObjectId,
+      ref: "Family",
+      required: true,
+    },
+    need: {
+      type: String,
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+    },
+    member: {
+      type: Schema.Types.ObjectId,
+      ref:"User",
+    },
+    price: {
+      type: Number,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: "version_key",
+  }
+);
+
+const Need = mongoose.models.Need || model("Need", needSchema);
+
+export default Need;
