5172e9deb8e198ed818e494ccab6795994917255
Merna Bassem <mernabassem21@gmail.com>
2024-08-24 21:36:47 +0300

    resource model

---
+++ DB/Models/resource.model.js
@@ -0,0 +1,43 @@
+/**
+ * name -- unique
+ * quantity
+ * type
+ * addedBy
+ */
+
+import mongoose from "mongoose";
+
+const { Schema, model } = mongoose;
+
+const resourceSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      minLength: 3,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    addedBy: {
+      type: Schema.Types.ObjectId,
+      ref: "Admin_Volunteers",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: "version_key",
+  }
+);
+
+const Resource = mongoose.models.Resource || model("Resource", resourceSchema);
+
+export default Resource;
